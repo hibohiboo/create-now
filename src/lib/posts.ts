614a327561690d40b1ff4be4161fe375f167dcd4
hibohiboo <swdice66@gmail.com>
2020-04-30 13:23:03 +0900

    チュートリアル markdown描画

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+import remark from 'remark'
+import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
@@ -58,16 +60,23 @@ export function getAllPostIds() {
     }
   })
 }
-export function getPostData(id) {
+export async function getPostData(id) {
   const fullPath = path.join(postsDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
   // 投稿のメタデータ部分を解析するために gray-matter を使う
   const matterResult = matter(fileContents)
 
-  // データを id と組み合わせる
+  // マークダウンを HTML 文字列に変換するために remark を使う
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content)
+  const contentHtml = processedContent.toString()
+
+  // データを id および contentHtml と組み合わせる
   return {
     id,
+    contentHtml,
     ...matterResult.data,
   }
 }
