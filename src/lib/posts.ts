6c060a4a177f844866b3e4a0c8722569d131d2d6
hibohiboo <swdice66@gmail.com>
2020-04-30 12:07:07 +0900

    prefetch サンプル

---
+++ src/lib/posts.ts
@@ -0,0 +1,35 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+const postsDirectory = path.join(process.cwd(), 'posts')
+
+export function getSortedPostsData() {
+  // /posts　配下のファイル名を取得する
+  const fileNames = fs.readdirSync(postsDirectory)
+  const allPostsData = fileNames.map((fileName) => {
+    // id を取得するためにファイル名から ".md" を削除する
+    const id = fileName.replace(/\.md$/, '')
+
+    // マークダウンファイルを文字列として読み取る
+    const fullPath = path.join(postsDirectory, fileName)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+    // 投稿のメタデータ部分を解析するために gray-matter を使う
+    const matterResult = matter(fileContents)
+
+    // データを id と合わせる
+    return {
+      id,
+      ...matterResult.data,
+    }
+  })
+  // 投稿を日付でソートする
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1
+    } else {
+      return -1
+    }
+  })
+}
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '~/components/templates/posts/Layout'
 import utilStyles from '~/styles/utils.module.scss'
+import { getSortedPostsData } from '~/lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -21,6 +22,28 @@ export default function Home() {
           </Link>
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
+}
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
 }
