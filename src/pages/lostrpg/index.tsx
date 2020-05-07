b7308c0362ea6713610181a363863ff7f45ef036
hibohiboo <swdice66@gmail.com>
2020-05-07 20:54:29 +0900

    導線の追加

--- src/pages/lostrpg/index.tsx
+++ src/pages/lostrpg/index.tsx
@@ -1,13 +1,22 @@
+import React, { useEffect } from 'react'
 import { NextPage } from 'next'
 import Head from 'next/head'
+import { useDispatch } from 'react-redux'
 import Link from '~/components/atoms/mui/Link'
 import Container from '~/components/organisms/lostrpg/LostrpgContainer'
 import useI18n from '~/hooks/use-i18n'
 import { contentLanguageMap } from '~/lib/i18n'
 import LanguageSelector from '~/components/organisms/i18n/LanguageSelector'
+import { useAuth, createAuthClientSide } from '~/store/modules/authModule'
 
 const Page: NextPage = () => {
   const i18n = useI18n()
+  const t = i18n.t
+  const authUser = useAuth()
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(createAuthClientSide())
+  }, [])
 
   return (
     <Container>
@@ -16,29 +25,48 @@ const Page: NextPage = () => {
           httpEquiv="content-language"
           content={contentLanguageMap[i18n.activeLocale]}
         />
-        <title>{i18n.t('lostrpg_index_title')}</title>
+        <title>{t('lostrpg_index_title')}</title>
       </Head>
-      <h2>{i18n.t('lostrpg_index_title')}</h2>
+      <h2>{t('lostrpg_index_title')}</h2>
       <LanguageSelector i18n={i18n} />
       <ul>
         <li>
-          <Link href="/lostrpg/camps/list">
-            {i18n.t('lostrpg_index_campList')}
-          </Link>
+          <Link href="/lostrpg/camps/list">{t('lostrpg_index_campList')}</Link>
         </li>
         <li>
           <Link
             href="/lostrpg/characters/[lng]/list"
             as={`/lostrpg/characters/${i18n.activeLocale}/list`}
           >
-            {i18n.t('lostrpg_index_characters')}
+            {t('lostrpg_index_characters')}
           </Link>
         </li>
       </ul>
+      {authUser ? (
+        <ul>
+          <li>
+            <Link href="/lostrpg/camps/edit">{`${t('lostrpg_common_camp')}${t(
+              'common_create',
+            )}`}</Link>
+          </li>
+          <li>
+            <Link
+              href={`/lostrpg/characters/[lng]/edit`}
+              as={`/lostrpg/characters/${i18n.activeLocale}/edit`}
+            >
+              {`${t('lostrpg_common_character')}${t('common_create')}`}
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <p>
+          <Link href="/auth/login">{t('lostrpg_camps_list_please_login')}</Link>
+        </p>
+      )}
+
       <div style={{ padding: '10px' }}>
         {i18n.t('lostrpg_index_please_translate')}
         <a
-          target="_blank"
           href="https://github.com/hibohiboo/create-now/blob/master/src/data/lostrpg-en.ts"
           rel="noopener noreferrer"
         >
