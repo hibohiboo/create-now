809ca78c353c3b705ae701349f6cf98def3a0ac4
hibohiboo <swdice66@gmail.com>
2021-05-27 22:14:49 +0900

    成功率追加

--- src/domain/kakuriyogarden/components/character/edit/organisms/Character.tsx
+++ src/domain/kakuriyogarden/components/character/edit/organisms/Character.tsx
@@ -4,6 +4,8 @@ import SubPage from '../layout/SubPage'
 import MagicalName from './character/MagicalName'
 import Profile from './character/Profile'
 import Hope from './character/Hope'
+import Card from './card'
+
 const Item: FC<{ vm: CharacterViewModel }> = ({ vm }) => {
   return (
     <SubPage id="cloth">
@@ -28,6 +30,30 @@ const Item: FC<{ vm: CharacterViewModel }> = ({ vm }) => {
         setHopeDetail={vm.setHopeDetail}
         hopeDetail={vm.character.hopeDetail}
         openInputModal={vm.openInputModal}
+      />
+      <Card
+        cardData={{
+          type: 'マギア',
+          kind: '魔法',
+          name: 'アベンジャー',
+          timing: 'アクション',
+          count: '5',
+          target: '単体',
+          range: '近接状態',
+          tags: ['願望'],
+          effect: '',
+          description: '',
+          id: '',
+          image: {
+            url: '/images/kakuriyogarden/icons/game-icons/spark-spirit.png',
+            source: 'Game-icons.net',
+            sourceUrl: 'https://game-icons.net/1x1/lorc/spark-spirit.html',
+          },
+          maxLevel: 1,
+          successRate: '-',
+          level: 1,
+          exp: '',
+        }}
       />
     </SubPage>
   )
---
+++ src/domain/kakuriyogarden/components/character/edit/organisms/card/index.tsx
@@ -0,0 +1,106 @@
+import { FC } from 'react'
+import {
+  OpenImageEditModal,
+  OpenInputModal,
+} from '~/domain/kakuriyogarden/store/character/modal'
+export type Card = {
+  type: string
+  kind: string
+  name: string
+  timing: string
+  count: string
+  target: string
+  range: string
+  tags: string[]
+  effect: string
+  description: string
+  id: string
+  image: null | { sourceUrl: string; url: string; source: string }
+  maxLevel: null | number
+  level: null | number
+  successRate: string
+  exp: null | string
+}
+const labelData = {
+  timing: 'タイミング',
+  count: 'カウント',
+  range: '射程',
+  target: '対象',
+  maxLevel: '最大Lv',
+  level: 'Lv',
+  exp: 'EXP',
+  successRate: '成功率',
+}
+
+const Card: FC<{ cardData: Card }> = ({ cardData }) => {
+  return (
+    <div style={{ padding: '0px', width: '252px', backgroundColor: '#fff' }}>
+      <div className="skill-card">
+        <div className="wrapper">
+          <div className="base">
+            <div className="skillLabel">{`${cardData.type}/${cardData.kind}`}</div>
+            <div className="image">
+              <img src={cardData.image.url} crossOrigin="use-credentials"></img>
+            </div>
+            <div className="cardName">{cardData.name}</div>
+
+            <div className="attrTimingLabel attrLabel border-tlr">
+              {labelData.timing}
+            </div>
+            <div className="attrTimingValue border-tr">{cardData.timing}</div>
+
+            <div className="attrCountLabel attrLabel border-blr">
+              {labelData.count}
+            </div>
+            <div className="attrCountValue border-br">{cardData.count}</div>
+            <div className="attrRangeLabel attrLabel border">
+              {labelData.range}
+            </div>
+            <div className="attrRangeValue border-trb">{cardData.range}</div>
+            <div className="attrTargetLabel attrLabel border-blr">
+              {labelData.target}
+            </div>
+            <div className="attrTargetValue border-br">{cardData.target}</div>
+            <div className="attrSuccessRateLabel attrLabel border-blr">
+              {labelData.successRate}
+            </div>
+            <div className="attrSuccessRate border-br">
+              {cardData.successRate}
+            </div>
+
+            {cardData.exp ? (
+              <>
+                <div className="attrExpLabel attrLabel border-blr">
+                  {labelData.exp}
+                </div>
+                <div className="attrExpValue border-br">{cardData.exp}</div>{' '}
+              </>
+            ) : (
+              <></>
+            )}
+            <div className="tags">
+              {cardData.tags.map((tag) => (
+                <span className="tag" key={tag}>
+                  {tag}
+                </span>
+              ))}
+            </div>
+            <div className="mainContent border">
+              <div className="effect">{cardData.effect}</div>
+              <div className="description">{cardData.description}</div>
+              <div className={['description', 'link'].join(' ')}></div>
+            </div>
+            <div className="bottomContent">
+              <div className="illustedBy">
+                <a
+                  href={cardData.image.sourceUrl}
+                >{`illust: ${cardData.image.source}`}</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Card
