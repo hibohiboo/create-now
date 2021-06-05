badfc22c7168902144d276c5662265398d20a951
hibohiboo <swdice66@gmail.com>
2021-06-05 10:25:03 +0900

    魔法作成

--- src/domain/kakuriyogarden/classes/gemory/magic.ts
+++ src/domain/kakuriyogarden/classes/gemory/magic.ts
@@ -34,7 +34,7 @@ export const moveMagic: Magic = {
   range: '自身',
   tags: ['移動', '即唱'],
   effect:
-    '[この魔法が配置されている階層]マスまで移動できる。',
+    '移動:[配置階層]。',
   gardeneffect: 'アクションで撤退できる。',
   description: '細道。',
   id: '',
@@ -47,6 +47,122 @@ export const moveMagic: Magic = {
   },
   maxLevel: 1,
   successRate: '100%',
+  level: 1,
+  exp: 15,
+}
+
+
+export const quickShootMagic: Magic = {
+  type: '魔法',
+  kind: '一般',
+  nameKana: '',
+  name: 'クイックシュート',
+  timing: 'アクション',
+  count: '2',
+  target: '単体',
+  range: '2',
+  tags: ['攻撃', '詠唱','一節'],
+  effect:
+    'ダメージ:1',
+  gardeneffect: '射程+1',
+  description: '細い光',
+  successRate: '50%',
+  id: '',
+  image: {
+    url:
+      '/images/kakuriyogarden/icons/game-icons/striking-splinter.svg',
+    source: 'Game-icons.net',
+    sourceUrl:
+      'https://game-icons.net/1x1/lorc/striking-splinter.html',
+  },
+  maxLevel: 1,
+
+  level: 1,
+  exp: 15,
+}
+export const shootMagic: Magic = {
+  type: '魔法',
+  kind: '一般',
+  nameKana: '',
+  name: 'シュート',
+  timing: 'アクション',
+  count: '4',
+  target: '単体',
+  range: '4',
+  tags: ['攻撃', '詠唱'],
+  effect:
+    'ダメージ:[配置階層]',
+  gardeneffect: '射程+1',
+  description: '強い光',
+  successRate: '70%',
+  id: '',
+  image: {
+    url:
+      '/images/kakuriyogarden/icons/game-icons/supersonic-arrow.svg',
+    source: 'Game-icons.net',
+    sourceUrl:
+      'https://game-icons.net/1x1/lorc/supersonic-arrow.html',
+  },
+  maxLevel: 1,
+
+  level: 1,
+  exp: 15,
+}
+export const wallMagic: Magic = {
+  type: '魔法',
+  kind: '一般',
+  nameKana: 'ウォール',
+  name: '防壁',
+  timing: '常駐',
+  count: '0',
+  target: '自身',
+  range: '自身',
+  tags: [],
+  effect:
+    `この魔法の配置階層より深い階層と、浅い階層（配置階層を含む）では互いに射程が届かないものとする。
+配置階層より深い階層へは庭園主しか入ることはできない。`,
+  gardeneffect: 'ダメージ軽減:1',
+  description: '心理防壁。何物モ立チ入ルベカラズ',
+  successRate: '70%',
+  id: '',
+  image: {
+    url:
+      '/images/kakuriyogarden/icons/game-icons/brick-wall.svg',
+    source: 'Game-icons.net',
+    sourceUrl:
+      'https://game-icons.net/1x1/lorc/brick-wall.html',
+  },
+  maxLevel: 1,
+
+  level: 1,
+  exp: 15,
+}
+
+export const gemory: Magic = {
+  type: '想晶',
+  kind: '想晶',
+  nameKana: 'ココロガラス',
+  name: '想晶',
+  timing: '常駐',
+  count: '0',
+  target: '自身',
+  range: '自身',
+  tags: [],
+  effect:
+    ``,
+  gardeneffect: `攻撃対象に配置階層の想晶を選ぶことができる`,
+  description: '記憶の結晶。想いの欠片。',
+  successRate: '-',
+  id: '',
+  image: {
+    url:
+      '/images/kakuriyogarden/icons/game-icons/crystal-growth.svg',
+    source: 'Game-icons.net',
+    sourceUrl:
+      'https://game-icons.net/1x1/lorc/crystal-growth.html',
+  },
+  maxLevel: 1,
+
   level: 1,
   exp: 15,
 }
--- src/pages/kakuriyogarden/characters/edit/index.tsx
+++ src/pages/kakuriyogarden/characters/edit/index.tsx
@@ -3,17 +3,20 @@ import { useDispatch } from 'react-redux'
 import { NextPage } from 'next'
 import Head from 'next/head'
 
+import fetch from 'isomorphic-unfetch'
 import Edit from '~/domain/kakuriyogarden/components/character/edit'
+import { Magic } from '~/domain/kakuriyogarden/classes/gemory/magic'
 
 const Page: NextPage<{
   id: string
   pageTitle: string
+  skills: any
 }> = function (ctx) {
   const dispatch = useDispatch()
 
   useEffect(() => {}, [])
 
-  const { id } = ctx
+  const { id, skills } = ctx
   return (
     <>
       <Head>
@@ -37,15 +40,100 @@ const Page: NextPage<{
         <title>{ctx.pageTitle}</title>
       </Head>
       <Edit>{id}</Edit>
+      <div>{JSON.stringify(skills)}</div>
     </>
   )
 }
-
-Page.getInitialProps = async ({ query }) => {
-  const id = query.id as string
-
-  return { id, pageTitle: 'カクリヨガーデン: キャラクター編集' }
+export const getStaticProps = async (context) => {
+  const postId = context.params?.id
+  const sheet = (context.query?.sheet ||
+    '1uI6lGPfsXfn77tKjLBzKaSl9lHrTsN5la-tTDo9gPZw') as string
+  const data = await getSheetData(sheet, 'cards', 'B2:S')
+  const skills = data.values.map(csvToCard)
+  return {
+    props: {
+      id: sheet,
+      pageTitle: 'カクリヨガーデン: キャラクター編集',
+      skills,
+    },
+    revalidate: 60, // ここを追加
+  }
 }
+
+const fetchUrl = 'https://sheets.googleapis.com/v4/spreadsheets'
+const key = process.env.GOOGLE_API_KEY
+export const getSheetData = async (
+  spreadId: string,
+  sheet: string,
+  range: string,
+): Promise<{ values: string[][] }> => {
+  const res = await fetch(
+    `${fetchUrl}/${spreadId}/values/${sheet}!${range}?key=${key}`,
+  )
+  if (res.status >= 400) {
+    return { values: [[]] }
+  }
+  const json = await res.json()
+  if (!json.values) {
+    console.log('can not get values', json)
+    return { values: [[]] }
+  }
+
+  return json as { values: string[][] }
+}
+
+const csvToCard: (items: any[]) => Magic = (items) => {
+  const [
+    name,
+    nameKana,
+    type,
+    kind,
+    exp,
+    timing,
+    count,
+    range,
+    target,
+    successRate,
+    effect,
+    gardeneffect,
+    description,
+    strTags,
+    url,
+    siteName,
+    siteUrl,
+  ] = items
+
+  const tags = strTags ? strTags.split(',') : []
+  const image = {
+    url,
+    source: siteName,
+    sourceUrl: siteUrl,
+  }
+  const maxLevel = 1
+  const level = 1
+  const id = '1'
+  return {
+    id,
+    name,
+    nameKana,
+    type,
+    timing,
+    count,
+    range,
+    target,
+    maxLevel,
+    level,
+    successRate,
+    effect,
+    description,
+    gardeneffect,
+    tags,
+    image,
+    kind,
+    exp,
+  }
+}
+
 // Page.getStaticPaths = getLocalePaths
 
 export default Page
